700dd9a47630e1cfeac2323ba58575acf34f078c
Tommaso Parigi <tommaso.parigi06@gmail.com>
2024-09-24 11:35:13 +0200

    Page completed

--- index.php
+++ index.php
@@ -7,7 +7,11 @@
 </head>
 <body>
     <?php
-        
+        $str = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit, fuga quae. Facere doloremque architecto debitis. Quos, praesentium. Quia accusantium voluptatum minima, nemo minus assumenda eum reiciendis eveniet blanditiis, similique mollitia?";
+        echo "<p style='color:green'>$str</p>";
+        echo "<p style='color:blue'>". strtoupper($str) ."</p>";
+        echo "<span style='color:red'>". strlen($str) ."</span>";
+        echo "<span style='color:yellow'>". str_word_count($str) ."</span>";
     ?>
 </body>
 </html>
